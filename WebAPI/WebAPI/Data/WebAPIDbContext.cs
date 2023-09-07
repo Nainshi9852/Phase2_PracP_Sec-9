4b03505789dd07d3efd79898ad796023ec7f6e5b
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-09-07 12:25:31 +0530

    PracticeProject4

---
+++ WebAPI/WebAPI/Data/WebAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIDbContext : DbContext
+    {
+        public WebAPIDbContext (DbContextOptions<WebAPIDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI.Models.Marks> Marks { get; set; } = default!;
+    }
+}
